c4175fe0b1d71c9961c0f3e4c3749b73b776d406
JuanF-Cano <juan.cano5@utp.edu.co>
2024-06-27 10:06:50 -0500

    some change

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ItemCard from "./components/ItemCard/ItemCard.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ItemCard
+      image="https://estaticos.elcolombiano.com/binrepository/770x702/0c0/1200d627/none/11101/CTTB/intensamente-2_43726637_20231110121500.jpg"
+      title="Intensa mente 2"
+      age="2024"
+      gender="Infantil"
+    />
   </React.StrictMode>,
 )
