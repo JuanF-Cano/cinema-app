71c10bdedea9c6229853f8f58d04447a5bc10c0f
JuanF-Cano <173503745+JuanF-Cano@users.noreply.github.com>
2024-06-27 14:09:41 -0500

    Revert "API CALLS"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './styles.css';
 
 function App() {
   return (
