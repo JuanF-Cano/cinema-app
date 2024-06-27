737d08194289af01a334092b6e2a2a9e9f7ccf55
sofia osorio <david.patino1@utp.edu.co>
2024-06-27 00:22:05 -0500

    API CALLS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './styles.css';
+import './App.css';
 
 function App() {
   return (
