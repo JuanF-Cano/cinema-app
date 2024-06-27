45acae571edc54dc5bc3fc5ef154863bcf1e3a91
sofia osorio <david.patino1@utp.edu.co>
2024-06-26 22:53:21 -0500

    GENERANDO ESTRUCTURA 1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles.css';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/tv-shows" element={<TVShowsPage />} />
+        <Route path="/people" element={<PeoplePage />} />
+        <Route path="/item/:id" element={<ItemInfoPage />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
