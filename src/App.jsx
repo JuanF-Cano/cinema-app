993cd1c91533ff19178e553fa48f5accbdb032e6
yeisonbetancur <yeison.betancur@utp.edu.co>
2024-06-27 14:39:50 -0500

    Ajustes de debuging

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   
   return (
     <div className='App'>
-      <div className="container-Input">
-        <input 
-        onChange={handleChange}
-        value={search} 
-        placeholder="Search by"></input>
-      </div>
+      <SearchBar/>
+      <p>hola</p>
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,22 +1,17 @@
 import React from "react"
-const [search,setSearch]= useState("");
-  const [result,setResult] = useState([])
- /* const petiGet=async()=>{
-    axios.get("http://files.tmdb.org/p/exports/adult_movie_ids_05_15_2024.json.gz")
-    .then(response=>{
-     setResult(response.data)
-    }).catch(error=>{
-      console.log(error)
-    })
-  }*/
+import { useState } from "react";
+
+  const SearchBar =()=>{
+
+  const [search,setSearch]= useState("");
+
   const handleChange=e=>{//esto utiliza el useState de setSearch para recibir input del usuario, search contiene el string del input del usuario
     setSearch (e.target.value)
     console.log("Busqueda: "+e.target.value)
   }
-//Esto es la barra de busqueda, al cambio del estado se llama handleChange
-const SearchBar =()=>{
+
     return(
-        <div className="SearchBar- container-Input">
+        <div className="SearchBar-container-Input">
         <input 
         onChange={handleChange}
         value={search} 
@@ -24,3 +19,4 @@ const SearchBar =()=>{
       </div>
     )
 }
+export default SearchBar
