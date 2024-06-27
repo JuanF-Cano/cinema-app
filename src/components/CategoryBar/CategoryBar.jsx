7f57bff854d9e75462382f553345854a0613f977
Esteban <160088637+esteban23052005@users.noreply.github.com>
2024-06-27 00:28:50 -0500

    Prueba incompleta de el componente Navbar

---
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -0,0 +1,24 @@
+import '../../StyleSheets/NavBar.css'
+
+function NavBar(){
+
+    const gato=['hola', 'como', 'estas']
+        return (
+            <section>
+            <h3>Filters</h3>
+            <div>
+                {gato.map((gato, index) => (
+                   <div key={index}>
+                        <input type="checkbox" id={`filterCategory${index}`} />
+                        <label htmlFor={`filterCategory${index}`}>
+                            {gato}
+                       </label>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+    
+}
+
+export default NavBar;
