5e83e57fb8465d8f6dcb1d969d77bd934887fb85
Esteban <160088637+esteban23052005@users.noreply.github.com>
2024-06-30 21:03:24 -0500

    implementacion searchbar y la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import HomePage from "./pages/HomePage";
 import ItemInfoPage from "./pages/ItemInfoPage";
 import SearchPage from "./pages/SearchPage";
-import HomePage from "./pages/HomePage";
 
 
 function App() {
