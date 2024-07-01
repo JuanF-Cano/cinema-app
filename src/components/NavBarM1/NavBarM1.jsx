6a26515c307b24d7185bf4ec1dd3e630ce6a4eb4
CanoJuanFer <173503745+JuanF-Cano@users.noreply.github.com>
2024-07-01 07:22:46 -0500

    Estilos finales home page

--- src/components/NavBarM1/NavBarM1.jsx
+++ src/components/NavBarM1/NavBarM1.jsx
@@ -7,10 +7,8 @@ function NavBarM1() {
   return (
     <nav>
       <div className='nav-bar-links'>
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/tv-shows">TV Shows</Link>
-        <Link to="/people">People</Link>
+        <Link className="link-nav" to="/"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="50px" 
+        fill="#e8eaed"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg></Link>
       </div>
       <SearchBarM1></SearchBarM1>
     </nav>
